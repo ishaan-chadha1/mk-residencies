ffb737396c4ab786b39d9bab60c275d042bd47e2
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-05-12 16:31:18 +0530

    Change in styling of Prop Carousel Component.

--- pages/index.js
+++ pages/index.js
@@ -110,7 +110,7 @@ export default function Home() {
                                         </PageTitle>
                                     </div>
                                     <div className="container mx-auto px-4 pt-10 sm:px-6 lg:px-8">
-                                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                                        <div className="grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 gap-6">
                                             {propertyData.map(
                                                 (property, index) => (
                                                     <PropCarousel
