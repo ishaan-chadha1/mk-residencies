e30756fa984ab7184199888360aa98fda3fd8f92
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-07-21 12:22:44 +0530

    Fix Corporate Accomodation Component for Mobile Responsiveness

--- components/FeaturedCollection/featured-collection.jsx
+++ components/FeaturedCollection/featured-collection.jsx
@@ -6,7 +6,7 @@ export function FeaturedCollection() {
             <h2 className="text-2xl font-semibold mb-4">
                 Corporate Accommodation Near
             </h2>
-            <div className="grid grid-cols-7 gap-5">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-5">
                 <div className="rounded-lg overflow-hidden">
                     <Image
                         alt="Strategic Location"
